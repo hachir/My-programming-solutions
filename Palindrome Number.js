6b7fbd806fcaf1321de3f9b9287b713204ac73eb
hachir <ha1991ha@yahoo.com>
2019-06-07 20:07:26 -0600

    updat- Palindrome Number

--- Palindrome Number.js
+++ Palindrome Number.js
@@ -1,3 +1,10 @@
+/**Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+Example 1:
+Input: 121
+Output: true
+*/
+
+
 var isPalindrome = function(x) {
   if (!isNaN(x)) {
       x = x.toString();
